13551e169d3da1f0f51b951710cd096a46c86001
taedori <wlminimal@gmail.com>
2017-06-12 16:17:35 -0700

    Add blog index page

--- home/models.py
+++ home/models.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.db import models
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import datetime
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel, PageChooserPanel
 from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsnippets.models import register_snippet
@@ -48,6 +49,100 @@ class SermonVideo(models.Model):
 
     def __str__(self):
         return "{} - {}".format(self.sermon_title, self.sermon_date)
+
+
+@register_snippet
+class Author(models.Model):
+    author_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="75x75 pixel"
+    )
+    author_name = models.CharField(max_length=30, default="Pastor Kim")
+    author_title = models.CharField(max_length=60, default="Eastern Church Senior Pastor")
+
+    def __str__(self):
+        return self.author_name
+
+
+class BlogPage(Page):
+    blog_title = models.TextField(default="Blog Title")
+    blog_date = models.DateTimeField(auto_now_add=True)
+    blog_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        related_name='+',
+        on_delete=models.SET_NULL,
+        help_text="Blog Image"
+    )
+    blog_content = RichTextField()
+
+    blog_author = models.ForeignKey(
+        "home.Author",
+        blank=False,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+
+    blog_button_text = models.CharField(max_length=50, default="Read more blog")
+    blog_button_link = models.ForeignKey(
+        "wagtailcore.Page",
+        blank=False,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    parent_page_types = ['home.BlogIndexPage']
+
+    content_panels = Page.content_panels + [
+        FieldPanel('blog_title'),
+        ImageChooserPanel('blog_image'),
+        SnippetChooserPanel('blog_author'),
+        FieldPanel('blog_content'),
+        FieldPanel('blog_button_text'),
+        PageChooserPanel('blog_button_link')
+    ]
+
+
+class BlogIndexPage(Page):
+    blog_hero_title = models.TextField(default="Blog")
+    blog_hero_subtitle = models.TextField(default="Subtitle for Blog")
+    blog_featured_name = models.CharField(max_length=100, default="Featured Blog")
+
+    subpage_types = ['home.BlogPage']
+
+    blogs = BlogPage.objects.all().order_by("-blog_date")
+
+    def get_context(self, request, *args, **kwargs):
+        blogs = self.blogs
+
+        paginator = Paginator(blogs, 9)
+        page = request.GET.get('page')
+
+        try:
+            blogs = paginator.page(page)
+        except PageNotAnInteger:
+            blogs = paginator.page(1)
+        except EmptyPage:
+            blogs = paginator.page(paginator.num_pages)
+
+        context = super(BlogIndexPage, self).get_context(request, *args, **kwargs)
+        context['blogs'] = blogs
+
+        return context
+
+    content_panels = Page.content_panels + [
+        FieldPanel('blog_hero_title'),
+        FieldPanel('blog_hero_subtitle'),
+        FieldPanel('blog_featured_name'),
+    ]
+
 
 
 class HomePage(Page):
@@ -144,11 +239,30 @@ class HomePage(Page):
         help_text="64x64 icon"
     )
 
+    # Sermon Videos
     sermon_video_main_title = models.CharField(max_length=80, default="Sermon Video")
     sermon_video_subtitle = models.CharField(max_length=80, default="Recent Sermon Video")
     sermon_video_description = models.TextField(default="Click play to watch video")
     sermon_video_button_text = models.CharField(max_length=30, default="Watch more")
     sermon_video_button_link = models.CharField(max_length=80, default="/watch")
+
+    # Blog
+    blog_main_title = models.CharField(max_length=80, default="Blog")
+    blog_subtitle = models.CharField(max_length=100, default="Message from pastor")
+    blog_description = models.TextField(default="Description for blog section")
+    blog_button_text = models.CharField(max_length=50, default="See more blogs")
+    blog_button_link = models.ForeignKey(
+        "wagtailcore.Page",
+        blank=False,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    @property
+    def latest_blogs(self):
+        latest_blog = BlogPage.objects.all().order_by('-blog_date')
+        return latest_blog
 
     @property
     def latest_sermons(self):
@@ -157,8 +271,10 @@ class HomePage(Page):
 
     def get_context(self, request, *args, **kwargs):
         sermons = self.latest_sermons
+        latest_blog = self.latest_blogs
         context = super(HomePage, self).get_context(request, *args, **kwargs)
         context['sermons'] = sermons
+        context['latest_blogs'] = latest_blog
 
         return context
 
@@ -272,6 +388,18 @@ class HomePage(Page):
                 FieldPanel('sermon_video_button_link', classname="col6"),
             ])
         ]),
+
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('blog_main_title', classname="col6"),
+                FieldPanel('blog_subtitle', classname="col6")
+                ])
+        ]),
+        FieldPanel('blog_description'),
+        FieldPanel('blog_button_text', classname="col6"),
+        PageChooserPanel('blog_button_link'),
+
+
 
     ]
 
